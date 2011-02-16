09ee75e8913c974a3923c96f0debb5a35e6dc8aa
gleontiev <gleontiev@Alonzo-Church.(none)>
2011-02-16 17:12:19 +0200

    Tab -> Space

--- src/com/folone/replscheme/SchemeREPL.java
+++ src/com/folone/replscheme/SchemeREPL.java
@@ -10,25 +10,25 @@ import android.os.RemoteException;
 
 public class SchemeREPL extends Service {
 
-	private final Evaluator.Stub evaluator = new Evaluator.Stub() {
-		
-	    private JScheme scheme = new JScheme();
-	    
-		public String evaluate(String script) throws RemoteException {
-			
-			String result = "Something went wrong.";
-			try {
-				result = scheme.eval(script).toString();
-			} catch (Exception e) {
-				result = e.getMessage();
-			}
-			return result;
-		}
-	};
-	
-	@Override
-	public IBinder onBind(Intent intent) {
-		return evaluator;
-	}
+    private final Evaluator.Stub evaluator = new Evaluator.Stub() {
+
+        private JScheme scheme = new JScheme();
+
+        public String evaluate(String script) throws RemoteException {
+
+            String result = "Something went wrong.";
+            try {
+                result = scheme.eval(script).toString();
+            } catch (Exception e) {
+                result = e.getMessage();
+            }
+            return result;
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return evaluator;
+    }
 
 }
